6aab0469b8315ade600ddc648e7e8cb0c915d863
dhirose <hiroki.940920@gmail.com>
2024-09-01 14:36:12 +0900

    GitHubからアクセストークンでリポジトリ一覧を取得するツールを作成

---
+++ conf/GitHub/GitHubApiConf.ps1
@@ -0,0 +1,4 @@
+# APIベースURL
+Set-Variable -Option Constant -Name C_GITHUB_API_BASE_URL -Value "https://api.github.com"
+# /user/repos
+Set-Variable -Option Constant -Name C_GITHUB_API_URI_GET_USER_REPOS -Value "/user/repos"
---
+++ conf/GitHub/GitHubConnectionConf.ps1
@@ -0,0 +1,4 @@
+# ユーザー名
+Set-Variable -Option Constant -Name C_GITHUB_USER_NAME -Value "user_name"
+# パーソナルアクセストークン
+Set-Variable -Option Constant -Name C_GITHUB_PERSONAL_ACCESS_TOKEN -Value "access_token"
